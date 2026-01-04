59775be885528313174812b0e6a8062bf8cab9fb
Liam L <thefallenspirit@outlook.com>
2026-01-04 07:38:24 -0800

    Add media logging

---
+++ src/events/messageCreate.ts
@@ -0,0 +1,25 @@
+import { createEvent } from 'seyfert';
+import { getGuild } from '../store.js';
+import MediaLog from '../models/MediaLog.js';
+
+export default createEvent({
+    data: { name: 'messageCreate' },
+    run: async (message) => {
+        if (!message.guildId) return;
+        const guildConfig = await getGuild(message.guildId);
+
+        if (!guildConfig) return;
+        const channel = await message.channel();
+
+        if (
+            guildConfig.mediaChannels?.includes(channel.id) ||
+            ('parentId' in channel) && channel.parentId && guildConfig.mediaChannels?.includes(channel.parentId)
+        ) await MediaLog.create({
+            guildId: message.guildId,
+            authorId: message.author.id,
+            channelId: message.channelId,
+            messageId: message.id
+        });
+    }
+});
+''
